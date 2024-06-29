3eb657f34ce6e0cdc6211b9f47eb3de2781cbfde
farhan-mazumder <farhanmazumder75@gmail.com>
2024-06-29 08:35:12 +0600

    add middleware for verify token, admin and user.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 const app = express();
 const prot = process.env.PORT || 5000;
 
-//middleware
+//middlewares
 app.use(
   cors({
     origin: ["http://localhost:5173"],
@@ -16,6 +16,49 @@ app.use(
 );
 app.use(express.json());
 
+const verifyToken = (req, res, next) => {
+  // console.log("line 20", req?.headers.authorization);
+  const token = req?.headers?.authorization.split(" ")[1];
+  // console.log(token);
+  if (!token) {
+    return res.status(401).send({ message: "Unauthorized access" });
+  }
+  // const token = req?.headers?.authorization.split(" ")[1];
+  // console.log(token);
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "Unauthorized access" });
+    }
+    req.decoded = decoded;
+
+    next();
+  });
+};
+
+const verifyUser = async (req, res, next) => {
+  const email = req.params?.email;
+  if (email !== req?.decoded?.email) {
+    return res.status(403).send({ message: "forbidden access" });
+  }
+  next();
+};
+const verifyAdmin = async (req, res, next) => {
+  // const email = req.params?.email;
+  // console.log("email", email);
+  // console.log("decoded", req?.decoded?.email);
+  // if (email !== req?.decoded?.email) {
+  //   return res.status(403).send({ message: "forbidden access" });
+  // }
+
+  const email = req?.decoded?.email;
+  const query = { email: email };
+  const user = await users_collection.findOne(query);
+  const isAdmin = user?.role === "admin";
+  if (!isAdmin) {
+    return res.status(403).send({ message: "forbidden access" });
+  }
+  next();
+};
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@bistro-boss-cluster.lkmirp3.mongodb.net/?retryWrites=true&w=majority&appName=BISTRO-BOSS-Cluster`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -39,8 +82,9 @@ async function run() {
     // Jwt related api's
     app.post("/jwt", (req, res) => {
       const user = req.body;
+      // console.log(user);
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: "1h",
+        expiresIn: "1d",
       });
       // console.log(token);
       res.send({ token });
@@ -60,7 +104,7 @@ async function run() {
     });
 
     // GET :: get users from users collection in database
-    app.get("/users", async (req, res) => {
+    app.get("/users", verifyToken, verifyAdmin, async (req, res) => {
       // console.log("test", req.headers);
       const result = await users_collection.find().toArray();
       res.send(result);
@@ -84,6 +128,30 @@ async function run() {
       };
       const result = await users_collection.updateOne(filter, updatedInfo);
       res.send(result);
+    });
+
+    // GET :: chack is the user admin or not from users collection in database
+    app.get("/user/admin/:email", verifyToken, verifyUser, async (req, res) => {
+      const email = req?.params?.email;
+      // console.log("email", email);
+      // console.log("decoded", req?.decoded?.email);
+      // if (email !== req?.decoded?.email) {
+      //   return res.status(403).send({ message: "forbidden accesss" });
+      // }
+      const query = { email: email };
+      let isAdmin = false;
+      // const result = await users_collection.findOne(query);
+      // console.log(result?.role);
+      // if (result?.role === "admin") {
+      //   isAdmin = true;
+      // }
+      const user = await users_collection.findOne(query);
+      // console.log("user", user);
+      if (user) {
+        isAdmin = user?.role === "admin";
+      }
+      // console.log(isAdmin);
+      res.send({ isAdmin });
     });
 
     // GET :: get menus from menus collection in database
